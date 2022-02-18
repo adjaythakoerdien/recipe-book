ef811fb2ba42c7775dd7e4cc0397c33409de31fe
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-02-18 06:57:58 +0100

    Save + Fetch functionality added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage.service';
+
 
 
 @Component({
@@ -16,9 +18,17 @@ export class HeaderComponent implements OnInit {
     this.featureSelected.emit(feature);
   }
 
-  constructor() { }
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit(){
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,25 +11,31 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Hamburger',
-      'Meeeaaaaat!',
-      'https://images.unsplash.com/photo-1598182198871-d3f4ab4fd181?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8aGFtYnVyZ2VyfHx8fHx8MTY0NDIxOTkyNA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=600',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Bun', 1)
-      ]),
-    new Recipe('Pasta',
-      'What else you need to say?',
-      'https://images.unsplash.com/photo-1615502732386-6be2dffffb1a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8cGFzdGF8fHx8fHwxNjQ0MjE5OTk0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=600',
-      [
-        new Ingredient('Pasta', 85),
-        new Ingredient('Something else', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Hamburger',
+  //     'Meeeaaaaat!',
+  //     'https://images.unsplash.com/photo-1598182198871-d3f4ab4fd181?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8aGFtYnVyZ2VyfHx8fHx8MTY0NDIxOTkyNA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=600',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Bun', 1)
+  //     ]),
+  //   new Recipe('Pasta',
+  //     'What else you need to say?',
+  //     'https://images.unsplash.com/photo-1615502732386-6be2dffffb1a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8cGFzdGF8fHx8fHwxNjQ0MjE5OTk0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=600',
+  //     [
+  //       new Ingredient('Pasta', 85),
+  //       new Ingredient('Something else', 1)
+  //     ])
+  // ];
 
+  private recipes: Recipe[];
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
