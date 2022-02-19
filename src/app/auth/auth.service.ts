a224b937bd336214c3452f15631bdaf618021bcf
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-02-19 14:42:11 +0100

    creating & storing user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,23 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError } from "rxjs/operators";
-import { throwError } from 'rxjs';
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from 'rxjs';
+import { User } from "./user.model";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -23,17 +27,52 @@ export class AuthService {
       password: password,
       returnSecurToken: true
     }
-   ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!'
+   ).pipe(
+     catchError(this.handleError),
+     tap(resData => {
+     this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+   })
+   );
+  }
 
-      if (!errorRes || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS': errorMessage = 'This email has already been used, klootzak!'; break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER': errorMessage = 'To many trys, please try again later'; break;
-      }
-      return throwError(errorMessage)
-    }));
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAhK8LRcpeXWXm79ZrWrugqzE6mmLoieJ8',
+    {
+      email: email,
+      password: password,
+      returnSecurToken: true
+    }
+    )
+    .pipe(
+      catchError(this.handleError),
+      tap(resData => {
+        this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+   })
+   );
+  }
+
+  private handleAuthentication(
+    email: string, 
+    userId: string, 
+    token: string, 
+    expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+     const user = new User(email, userId, token, expirationDate);
+     this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!'
+
+    if (!errorRes || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS': errorMessage = 'This email has already been used, klootzak!'; break;
+      case 'EMAIL_NOT_FOUND': errorMessage = 'Invalid email/password'; break;
+      case 'INVALID_PASSWORD': errorMessage = 'Invalid email/password'; break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER': errorMessage = 'To many trys, please try again later'; break;
+    }
+    return throwError(errorMessage)
   }
 }
